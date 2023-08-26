08328f1afc7f6c328b76d2d77b9e1a7d313f3005
jay75chauhan <chauhan75jayk@gmail.com>
2023-08-26 17:43:00 +0530

    aws cli rs tr added

---
+++ aws_resource_tracker.sh
@@ -0,0 +1,16 @@
+
+#list s3 bukets
+echo "List of S3 buckets:"
+aws s3 ls
+
+#list lambda functions
+echo "List of Lambda functions:"
+aws lambda list-functions
+
+# list ec2 instances
+echo "List of EC2 instances:"
+aws ec2 describe-instances | jq -r '.Reservations[].Instances[].InstanceId'
+
+# IAM user
+echo "List of IAM users:"
+aws iam list-users | jq -r '.Users[].UserName'
